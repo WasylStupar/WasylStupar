fe0263ed5e89d8de56673ec9cd080f9600c864ee
WasylStupar <163508047+WasylStupar@users.noreply.github.com>
2024-05-22 00:30:01 +0300

    Add files via upload

---
+++ pr11/js11.js
@@ -0,0 +1,23 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var meal = document.querySelector('input[name="meal"]:checked');
+    var dishes = document.querySelectorAll('input[name="dish"]:checked');
+    var comment = document.getElementById('comment').value;
+    var resultDiv = document.getElementById('result');
+
+    if (username === '' || email === '' || meal === null || dishes.length === 0 || comment === '') {
+        alert('Будь ласка, заповніть всі поля');
+        return;
+    }
+
+    var mealValue = meal.value;
+    var dishValues = [];
+    dishes.forEach(function(checkbox) {
+        dishValues.push(checkbox.value);
+    });
+
+    var resultText = `${username} (${email}) з'їв на ${mealValue} ${dishValues.join(', ')} та залишив такий коментар: "${comment}"`;
+    resultDiv.textContent = resultText;
+});
+
